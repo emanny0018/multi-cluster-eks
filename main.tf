fb016feb4fb50de1f54684ffa436c1beb515d572
Eric Iyen <ericiyen@Erics-MacBook-Air.local>
2024-10-20 12:31:46 -0400

    newly updated files

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "etl-pipeline-manny"
     key            = "${terraform.workspace}/terraform.tfstate"  # Workspace-specific state file
     region         = "us-east-2"
-    dynamodb_table = "terraform-locks"
+    dynamodb_table = "tf_state_lock"
     encrypt        = true
   }
 }
